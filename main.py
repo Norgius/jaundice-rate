7f31d9bac2148b762754d4fe393a5207f5ed5b90
Norgius <dmitry.schadrin21@yandex.ru>
2023-06-23 18:52:57 +0300

    Catch exceptions

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import logging
 from enum import Enum
-from adapters.exceptions import ArticleNotFound
+from time import monotonic
+from pprint import pprint
 
 import aiohttp
 import asyncio
 import pymorphy2
 import aiofiles
+from async_timeout import timeout
 from anyio import create_task_group
 
+from adapters.exceptions import ArticleNotFound
 from adapters.inosmi_ru import sanitize
 from text_tools import split_by_words, calculate_jaundice_rate
+
+logger = logging.getLogger(__name__)
 
 TEST_ARTICLES = [
         'https://inosmi.ru/20230619/raketa-263752174.html',
@@ -16,13 +22,16 @@ TEST_ARTICLES = [
         'https://inosmi.ru/20221231/nauka-259263272.html',
         'http://example.com',
         'https://inosmi.ru/20230611/antropologiya-263558745.html',
-        'https://inosmi.ru/20230521/egipet-262991012.html'
+        'https://inosmi.ru/20230521/egipet-262991012.html',
+        'https://inosmi.ru/20230508/mozg-262641035.html',
 ]
 
 
 class ProcessingStatus(Enum):
     OK = 'OK'
     FETCH_ERROR = 'FETCH_ERROR'
+    PARSING_ERROR = 'PARSING_ERROR'
+    TIMEOUT = 'TIMEOUT'
 
 
 async def fetch(session: aiohttp.ClientSession, url: str):
@@ -33,25 +42,44 @@ async def fetch(session: aiohttp.ClientSession, url: str):
 
 async def process_article(session, morph, charged_words, url, processed_articles):
     try:
-        html = await fetch(session, url)
-        status = ProcessingStatus.OK.value
+        async with timeout(3):
+            html = await fetch(session, url)
         text = sanitize(html, True)
-        words = split_by_words(morph, text)
-        words_count = len(words)
-        score = calculate_jaundice_rate(words, charged_words)
-    except (aiohttp.ClientError, ArticleNotFound):
+
+        start = monotonic()
+        async with split_by_words(morph, text) as words:
+            words_count = len(words)
+            score = calculate_jaundice_rate(words, charged_words)
+        status = ProcessingStatus.OK.value
+        analysis_time = monotonic() - start
+        logger.info(f'Анализ закончен за {analysis_time} сек')
+
+    except aiohttp.ClientError:
         status = ProcessingStatus.FETCH_ERROR.value
         words_count, score = None, None
+    except ArticleNotFound:
+        status = ProcessingStatus.PARSING_ERROR.value
+        words_count, score = None, None
+    except asyncio.exceptions.TimeoutError:
+        status = ProcessingStatus.TIMEOUT.value
+        words_count, score = None, None
+
     results = {
         'url': url,
         'status': status,
         'words_count': words_count,
-        'score': score
+        'score': score,
     }
     processed_articles.append(results)
 
 
 async def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    logger.setLevel(logging.INFO)
+
     processed_articles = []
     morph = pymorphy2.MorphAnalyzer()
     async with aiofiles.open('charged_dict/positive_words.txt', 'r') as f:
@@ -63,7 +91,7 @@ async def main():
                     process_article,
                     *(session, morph, charged_words, url, processed_articles)
                 )
-    print(processed_articles)
+    pprint(processed_articles)
 
 
 asyncio.run(main())
--- text_tools.py
+++ text_tools.py
@@ -1,5 +1,11 @@
+import asyncio
+from time import monotonic
+from contextlib import asynccontextmanager
+
+from pytest_asyncio.plugin import pytest
 import pymorphy2
 import string
+import requests
 
 
 def _clean_word(word):
@@ -9,25 +15,38 @@ def _clean_word(word):
     return word
 
 
-def split_by_words(morph, text):
+@asynccontextmanager
+async def split_by_words(morph, text):
     """Учитывает знаки пунктуации, регистр и словоформы, выкидывает предлоги."""
+    start = monotonic()
     words = []
     for word in text.split():
         cleaned_word = _clean_word(word)
         normalized_word = morph.parse(cleaned_word)[0].normal_form
         if len(normalized_word) > 2 or normalized_word == 'не':
             words.append(normalized_word)
-    return words
+        if monotonic() - start > 3:
+            raise asyncio.exceptions.TimeoutError
+    yield words
 
 
-def test_split_by_words():
+@pytest.mark.asyncio
+async def test_split_by_words():
     # Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
     # Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
     morph = pymorphy2.MorphAnalyzer()
 
-    assert split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
+    async with split_by_words(morph, 'Во-первых, он хочет, чтобы') as res:
+        assert res == ['во-первых', 'хотеть', 'чтобы']
 
-    assert split_by_words(morph, '«Удивительно, но это стало началом!»') == ['удивительно', 'это', 'стать', 'начало']
+    async with split_by_words(morph, '«Удивительно, но это стало началом!»') as res:
+        assert res == ['удивительно', 'это', 'стать', 'начало']
+
+    response = requests.get('https://dvmn.org/media/filer_public/51/83/51830f54-7ec7-4702-847b-c5790ed3724c/gogol_nikolay_taras_bulba_-_bookscafenet.txt')
+    response.raise_for_status()
+    with pytest.raises(asyncio.exceptions.TimeoutError):
+        async with split_by_words(morph, response.text) as res:
+            pass
 
 
 def calculate_jaundice_rate(article_words, charged_words):
